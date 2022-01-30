241741cef72b431273370fa837f114cf52f15246
Thomas Kerby <63968823+TXOG@users.noreply.github.com>
2022-01-30 11:49:43 +0000

    main.py v1.0

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import requests
-from bs4 import BeautifulSoup
 import webbrowser
-from urllib.request import urlopen
-import urllib
-import re
-from requests_html import HTMLSession
 from selenium import webdriver
-from seleniumwire import webdriver
-import wget
+import time
+import json
+from selenium.webdriver.chrome.options import Options
+
+global pfp
+pfp = ()
 
 
 def target_id_enter():
@@ -21,38 +20,37 @@ def target_id_enter():
 
 
 def discord_id_website():
+    print("Finding user url")
+    global pfp
     global target_id
-    session = HTMLSession()
-    WEBDRIVER_PATH = './'
-    driver = webdriver.Firefox(WEBDRIVER_PATH)
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options=chrome_options)
     begining_of_url = "https://lookup.guru/"
     whole_url = begining_of_url + str(target_id)
     driver.get(whole_url)
-    #webbrowser.open(whole_url)
-    page = session.get(whole_url)
-    page.html.render()
-    page.html.html
-    print(driver.title)
-    proxy_username = "USER_NAME"
-    proxy_password = "PASSWORD"
-    proxy_url = "Any Website URL"
-    proxy_port = 8080
+    print("Waiting for lookup.guru load")
+    time.sleep(5)
+    print("Scraping pfp")
     images = driver.find_elements_by_tag_name('img')
-    options = {
-        "proxy": {
-            "http": f"http://{proxy_username}:{proxy_password}@{proxy_url}:{proxy_port}",
-            "verify_ssl": False,
-            },
-            }
     for image in images:
         global pfp
         pfp = (image.get_attribute('src'))
         break
-    print(pfp)
-    #763797441275232307
-    #img_data = requests.get(pfp).content
-    #with open('icon.png', 'wb') as handler:
-    #handler.write(img_data)
-    
+    img_data = requests.get(pfp).content
+    with open('pfpimage.png', 'wb') as handler:
+        handler.write(img_data)
+    print("Searching on yandex images")
+    filePath = "pfpimage.png"
+    searchUrl = 'https://yandex.com/images/search'
+    files = {'upfile': ('blob', open(filePath, 'rb'), 'image/jpeg')}
+    params = {'rpt': 'imageview', 'format': 'json',
+              'request': '{"blocks":[{"block":"b-page_type_search-by-image__link"}]}'}
+    response = requests.post(searchUrl, params=params, files=files)
+    query_string = json.loads(response.content)['blocks'][0]['params']['url']
+    img_search_url = searchUrl + '?' + query_string
+    print("Opening reverse image search")
+    webbrowser.open(img_search_url)
+
 
 target_id_enter()
