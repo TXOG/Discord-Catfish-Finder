dde166cb77d451ecfbba39ed26a534507c6b6936
Thomas Kerby <63968823+TXOG@users.noreply.github.com>
2022-02-02 13:10:40 +0000

    Add files via upload

---
+++ Source Code/Linux/V1.1/runme.sh
@@ -0,0 +1,8 @@
+green=`tput setaf 2`
+reset=`tput sgr0`
+echo "${green}installing colorama...${reset}"
+pip install colorama
+echo "${green}installing requests...${reset}"
+pip install requests
+echo "${green}installing selenium...${reset}"
+pip install selenium
